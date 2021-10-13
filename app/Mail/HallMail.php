dc22d9d6aa24584332507a27beb1873b40adece3
MAWcompany <45950499+Harbecox@users.noreply.github.com>
2021-10-13 15:28:10 +0400

    fix

--- app/Mail/HallMail.php
+++ app/Mail/HallMail.php
@@ -23,7 +23,7 @@ class HallMail extends Mailable
     }
 
     function telegram(){
-        $msg = $this->urls['front']."\n";
+        $msg = $this->urls['front']."\r\n";
         $msg.= $this->urls['admin'];
         $url = 'https://api.telegram.org/bot2087911716:AAHhXCxyMnVb8c1QmstHmiNyusDynGv_JI4/sendMessage?chat_id=-672072736&text='.$msg;
         file_get_contents($url);
