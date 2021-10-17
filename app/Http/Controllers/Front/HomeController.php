713560aeb28374ade7c267d2ca39a7aaf1b9ff38
MAWcompany <45950499+Harbecox@users.noreply.github.com>
2021-10-17 14:00:31 +0400

    fix

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -19,6 +19,7 @@ class HomeController extends Controller
             return $item->item;
         });
 
+        
         $arrays['companies'] = CompanyResource::collection(User::query()->whereIn("id", $arrays['companies'])->get());
         $arrays['halls'] = HallResource::collection(Hall::query()->whereIn("id", $arrays['halls'])->get());
         $arrays['services'] = HallResource::collection(Service::query()->whereIn("id", $arrays['services'])->get());
